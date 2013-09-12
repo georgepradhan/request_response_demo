d5da624005c18ad509aac3b71e01251f2d061409
George Pradhan <kgeorgepradhan@gmail.com>
2013-09-12 10:51:58 -0700

    change name of variable to server_data

--- public/js/application.js
+++ public/js/application.js
@@ -2,9 +2,9 @@ $(document).ready(function() {
   $('#ajax_link').click(function(e){
     e.preventDefault()
     console.log('clicked link')
-    $.get('/ajax', function(data){
+    $.get('/ajax', function(server_data){
       console.log('submitted ajax')
-      alert(data)
+      alert(server_data)
     })
   })
 });
